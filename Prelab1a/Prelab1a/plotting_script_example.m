0bec61fcc26078b4a0923a1988d2bfe8c46226af
rboone <rboone125@gmail.com>
2019-10-14 08:26:39 -0700

    work part complete for prelab 1a.  Need to find tau and finish report

--- Prelab1a/Prelab1a/plotting_script_example.m
+++ Prelab1a/Prelab1a/plotting_script_example.m
@@ -1,5 +1,12 @@
 %% First, extract variables of interest from the "struct"
 % where they are stored in the "MATLAB workspace":
+Kp = 20;
+Kd = 10;
+T = 0.005;
+f = 0.9;
+A = (1-f)/T;
+L1 = 11.2;
+L2 = 8;
 
 t = my_data.time;
 
@@ -10,18 +17,54 @@ t = my_data.time;
 ang = my_data.signals.values(:,1);
 des = my_data.signals.values(:,2);
 cvel = my_data.signals.values(:,3);
-ang2 = my_data.signals.values(:,4);
+%ang2 = my_data.signals.values(:,4);
+ang2 = my_data.signals.values(:,5);
+des2 = my_data.signals.values(:,6);
 
+yloc = zeros(10001, 1);
+xloc = zeros(10001, 1);
+ylocref = zeros(10001, 1);
+xlocref = zeros(10001, 1);
+
+
+for i=1:10000
+    yloc(i) = forwardKY(ang(i), ang2(i), L1, L2);
+    xloc(i) = forwardKX(ang(i), ang2(i), L1, L2);
+    ylocref(i) = forwardKY(des(i), des2(i), L1, L2);
+    xlocref(i) = forwardKX(des(i), des2(i), L1, L2);
+end
 figure(11); clf
-plot(t,des,'b-');
-hold on
-plot(t,ang,'r.');
-hold on
-plot(t,cvel,'g-');
-legend('My Trajectory','Motor Angle (rad)')
+if 0
+    plot(t,des,'b-');
+    hold on
+    plot(t,ang,'b.');
+    hold on
+    plot(t, des2, 'r-');
+    hold on
+    plot(t, ang2, 'r.');
+    legend('My Trajectory Motor 1','Motor 1 Angle (rad)', 'My Trajectory Motor 2','Motor 2 Angle (rad)')
+elseif 1
+    plot(xloc, yloc,'b-');
+    hold on
+    plot(xlocref, ylocref, 'r-');
+    legend('My EE Trajectory', 'Reference EE Trajectory');
+end
+% hold off
+% hold on
+% plot(t,cvel,'g-');
+%hold on
+%plot(t, ang2, 'y-');
 title('From: plotting\_script\_example.m')
 xlabel('Time (s)')
 ylabel('[Here, we have mixed units...]')
 grid on
 
 set(gca,'FontSize',14); % Easier to read, in a print-out...
+
+
+function y = forwardKY(theta1, theta2, L1, L2)
+    y = (L1 * sin(theta1)) + (L2 * sin(theta1 + theta2));
+end
+function x = forwardKX(theta1, theta2, L1, L2)
+   x = L1 * cos(theta1) + L2 * cos(theta1 + theta2);
+end
